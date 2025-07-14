5bd40238e71e4f4a0fe3992bd770bff20df3e503
Naftali Ferreira <108247621+naftaliferreira@users.noreply.github.com>
2025-07-14 19:21:30 -0300

    Update script.js
    Atualização e pequenas correções no código Javascript

--- script.js
+++ script.js
@@ -1,67 +1,98 @@
-const apiKeyInput = document.getElementById('apiKey');
-const gameSelect = document.getElementById('gameSelect');
-const questionInput = document.getElementById('questionInput');
-const askButton = document.getElementById('askButton');
-const aiResponse = document.getElementById('aiResponse');
-const form = document.getElementById('form');
+const apiKeyInput = document.getElementById('apiKey')
+const gameSelect = document.getElementById('gameSelect')
+const questionInput = document.getElementById('questionInput')
+const askButton = document.getElementById('askButton')
+const aiResponse = document.getElementById('aiResponse')
+const form = document.getElementById('form')
+
+const markdownToHTML = (text) => {
+    const converter = new showdown.Converter()
+    return converter.makeHtml(text)
+}
 
 const perguntarAI = async (question, game, apiKey) => {
-    const model = "gemini-2.0-flash"; // trocar nome do modelo Gemini quando for necessário usar versão mais atualizada
-    const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?=${apiKey}`;
+    const model = "gemini-2.0-flash"
+    const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`
     const pergunta = `
-        olha, tenho esse jogo ${game} e queria saber ${question}
-    `;
+    ## Especialidade
+    Você é um especialista assistente de meta para o jogo ${game}
+
+    ## Tarefa
+    Você deve responder as perguntas do usuário com base no seu conhecimento do jogo, estratégias, build e dicas
+
+    ## Regras
+    - Se você não sabe a resposta, responda com 'Não sei' e não tente inventar uma resposta.
+    - Se a pergunta não está relacionada ao jogo, responda com 'Essa pergunta não está relacionada ao jogo'
+    - Considere a data atual ${new Date().toLocaleDateString()}
+    - Faça pesquisas atualizadas sobre o patch atual, baseado na data atual, para dar uma resposta coerente.
+    - Nunca responsda itens que vc não tenha certeza de que existe no patch atual.
+
+    ## Resposta
+    - Economize na resposta, seja direto e responda no máximo 500 caracteres
+    - Responda em markdown
+    - Não precisa fazer nenhuma saudação ou despedida, apenas responda o que o usuário está querendo.
+
+    ## Exemplo de resposta
+    pergunta do usuário: Melhor build rengar jungle
+    resposta: A build mais atual é: \n\n **Itens:**\n\n coloque os itens aqui.\n\n**Runas:**\n\nexemplo de runas\n\n
+
+    ---
+    Aqui está a pergunta do usuário: ${question}
+  `
 
     const contents = [{
-       parts: [{
-        text: pergunta
-       }] 
-    }];
+        role: "user",
+        parts: [{
+            text: pergunta
+        }]
+    }]
 
-    // Chamada API
+    const tools = [{
+        google_search: {}
+    }]
+
+    // chamada API
     const response = await fetch(geminiURL, {
-        method: 'POST', 
+        method: 'POST',
         headers: {
-            'content-type': 'application/json'
-        }, 
+            'Content-Type': 'application/json'
+        },
         body: JSON.stringify({
-            contents:
+            contents,
+            tools
         })
     })
 
-    const data = await response.json();
-    console.log()
-    return 
+    const data = await response.json()
+    return data.candidates[0].content.parts[0].text
 }
 
 const enviarFormulario = async (event) => {
-    event.preventDefault();
-    const apiKey = apiKeyInput.value;
-    const game = gameSelect.value;
-    const question = questionInput.value;
+    event.preventDefault()
+    const apiKey = apiKeyInput.value
+    const game = gameSelect.value
+    const question = questionInput.value
 
-    if(apiKey == '' || game == '' || question == '') {
-        alert('Por favor, preencha todos os campos');
-        return 
-    } 
+    if (apiKey == '' || game == '' || question == '') {
+        alert('Por favor, preencha todos os campos')
+        return
+    }
 
-    askButton.disabled = true;
+    askButton.disabled = true
     askButton.textContent = 'Perguntando...'
-    askButton.classList.add('loading');
+    askButton.classList.add('loading')
 
     try {
-        // perguntar para a IA
-        await perguntarAI();
-
-    } catch(error) {
+        const text = await perguntarAI(question, game, apiKey)
+        aiResponse.querySelector('.response-content').innerHTML = markdownToHTML(text)
+        aiResponse.classList.remove('hidden')
+    } catch (error) {
         console.log('Erro: ', error)
-
     } finally {
-        askButton.disabled = false;
-        askButton.textContent = 'Perguntar';
-        askButton.classList.remove('loading');
+        askButton.disabled = false
+        askButton.textContent = "Perguntar"
+        askButton.classList.remove('loading')
     }
 }
-form.addEventListener('submit', enviarFormulario)
 
-perguntarAI(question, game, apiKey);
+form.addEventListener('submit', enviarFormulario)
